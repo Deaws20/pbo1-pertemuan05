6999fe54b84f86a009092f309f932cb4a8aad9c7
Deaws20 <d.salsabyla@gmail.com>
2020-04-30 13:00:23 +0800

    CLASS OBJECT RAMADHAN

--- src/Ramadhan.java
+++ src/Ramadhan.java
@@ -1,2 +1,73 @@
 public class Ramadhan {
+
+    //Attribut
+    String hambaAllah, derajat, ujianHidup, Rezeki;
+    int pahala;
+
+
+    //Behavior
+    void sholatTahajjud(){
+        pahala += 3000;
+        Rezeki = "Hadiah Mobil Impian";
+        ujianHidup = "dimarahi tetangga";
+        System.out.println("Ngantuk... Tapi Harus Bangun!");
+    }
+
+    void bacaQuran(){
+        pahala += 500;
+        Rezeki = "Takjil dari tetangga";
+        ujianHidup = "mati Lampu";
+        System.out.println("1 Day 1 Juz");
+    }
+
+    void sedekah(Ramadhan ramadhan){
+        pahala += 1000;
+        Rezeki = "THR 10jt";
+        ujianHidup = "anak perusahaan bangkrut";
+        System.out.println("Mengeluarkannya sambil merem");
+    }
+
+    void puasa(){
+        pahala += 2000;
+        Rezeki = "Baju Lebaran Baru, Alhamdulillah...";
+        ujianHidup = "Liat anak kecil makan es krim";
+        System.out.println("panas... haus... laper... Harus Sabar:) ");
+    }
+
+    void sholatTarawih(){
+        pahala += 800;
+        Rezeki = "Mudik Gratis";
+        ujianHidup = "mau pergi ke masjid, eh hujan";
+        System.out.println("Day 1  : keluar dosanya, sebagaimana ia baru dilahirkan dari perut ibunya");
+        System.out.println("Day 2  : diampuni dosanya dan kedua orangtuanya");
+        System.out.println("Day 3  : Malaikat memohonkan ampunan kepada Allah swt atas dosa-dosanya");
+        System.out.println("Day 4  : Pahala seperti membaca Taurat, Zabur, Injil, dan Al-Quran");
+        System.out.println("Day 5  : Pahala bagai sholat di Masjidil Haram, Masjid Nabawi, dan Masjidil Aqsha");
+        System.out.println("Day 6  : Pahala bagai tawaf di Baitul Makmur");
+        System.out.println("Day 7  : Pahala bagai berada di zaman Nabi Musa as dan membantu menyelamatkannya");
+        System.out.println("Day 8  : Diberi Anugerah bagai Anugerah Nabi Ibrahim As");
+        System.out.println("Day 9  : Pahala ibadah bagai ibadah para Nabi As");
+        System.out.println("Day 10 : Mendapat Rezeki yang lebih baik dari Rezeki yang ada di dunia maupun akhirat");
+        System.out.println("Day 11 : dimudahkan saat sakaratul maut, bagai baru dilahirkan dari perut ibunya");
+        System.out.println("Day 12 : dibangkitkan dengan wajah bagai bulan purnama");
+        System.out.println("Day 13 : Akan diselamatkan dari segala macam keburukan");
+        System.out.println("Day 14 : dibebaskan dari hisab di akhirat kelak");
+        System.out.println("Day 15 : dimintakan ampunan oleh para malaikat");
+        System.out.println("Day 16 : dibebaskan dari api neraka dan dimasukkan ke surga");
+        System.out.println("Day 17 : diberi pahala bagai pahala para Nabi As");
+        System.out.println("Day 18 : diserukan oleh para malaikat, bahwa Allah swt telah meridhoi");
+        System.out.println("Day 19 : diangkat derjatnya di surga firdaus");
+        System.out.println("Day 20 : Pahala seperti orang sholeh dan mati syahid");
+        System.out.println("Day 21 : dibuatkan rumah yang terbuat dari cahaya di surga");
+        System.out.println("Day 22 : Akan diselamatkan dari kesusahan dan kebingungan di hari kiamat");
+        System.out.println("Day 23 : dibangunkan kota di dalam surga");
+        System.out.println("Day 24 : diqabulkan 24 doanya");
+        System.out.println("Day 25 : dihilangkan siksa kuburnya");
+        System.out.println("Day 26 : ditingkatkan pahalanya selama 40 tahun");
+        System.out.println("Day 27 : melewati titian Sirathal Mustaqim secepat kilat");
+        System.out.println("Day 28 : diangkat seribu derajat di dalam surga");
+        System.out.println("Day 29 : Pahala bagai seribu kali ibadah haji yang diterima");
+        System.out.println("Day 30 : dipersilahkan masuk ke surga dan diberikan nikmat tak terkira disyurga");
+    }
+
 }
